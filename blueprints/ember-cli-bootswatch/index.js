05502c29ab3e5305fb944ed8e5ca7450638c365b
Ryan Panning <panman82@gmail.com>
2014-12-23 00:16:04 -0600

    Enable running blueprint without entity name
    Missed a comma after description and fix issue running blueprint
    without an entity name. Fixes #6

--- blueprints/ember-cli-bootswatch/index.js
+++ blueprints/ember-cli-bootswatch/index.js
@@ -1,5 +1,11 @@
 module.exports = {
-  description: 'Add bower dependencies for bootstrap and bootswatch to the project'
+  description: 'Add bower dependencies for bootstrap and bootswatch to the project',
+
+  normalizeEntityName: function() {
+    // allows us to run ember -g ember-cli-bootswatch and not blow up
+    // because ember cli normally expects the format
+    // ember generate <entitiyName> <blueprint>
+  },
 
   afterInstall: function(options) {
     return this.addBowerPackagesToProject([

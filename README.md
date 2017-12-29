57abe9ebc1db4ca0333d09e4e8cc3031a25f1c87
Ryan Panning <panman82@gmail.com>
2017-12-29 17:32:37 -0600

    Import from npm instead of bower and upgrade to bootstrap 4
    Fixes #14 #16

--- README.md
+++ README.md
@@ -2,29 +2,38 @@ ember-cli-bootswatch [![Ember Observer Score](https://emberobserver.com/badges/e
 ====================
 
 An [ember-cli addon](http://www.emberaddons.com/) to import a [Bootswatch](http://bootswatch.com/)
-or [Bootstrap](http://getbootstrap.com/) theme, including the fonts and
-JavaScript. This addon is only meant to import the related bower files and
-does NOT contain [Ember Components](https://guides.emberjs.com/v2.13.0/components/defining-a-component/)
-to use within your templates. Other addons provide those features,
-[look at emberobserver.com](https://www.emberobserver.com/categories/bootstrap) for
-those.
+theme or the [Bootstrap](http://getbootstrap.com/) theme, including the JavaScript plugins if desired.
+This addon is only meant to import the appropriate assets and does NOT contain
+[Ember Components](https://guides.emberjs.com/v2.17.0/components/defining-a-component/)
+to use within your templates. Other addons provide those features, such as
+[ember-bootstrap](http://www.ember-bootstrap.com/) or
+[visit emberobserver.com](https://www.emberobserver.com/categories/bootstrap) for others.
 
 _Note, this addon scores low on [Ember Observer](https://emberobserver.com/addons/ember-cli-bootswatch) because; it's very basic and isn't easily testable (-2 points), and it could use more contributers (-1 point). It is still maintained and works with the latest versions of ember-cli (2.x)!_
 
 
 
 
+## Compatibility
+
+This addon has a version break for the Bootstrap version and ember-cli requirements.
+
+| Addon Version | Bootstrap Version | ember-cli Version | Dependencies    |
+|---------------|-------------------|-------------------|-----------------|
+| 1.x           | 3.x               | 1.13+             | Uses bower deps |
+| 2.x           | 4.x               | 2.15+             | Uses npm deps   |
+
+
+
+
 ## Installation
 
-From within your [ember-cli](http://www.ember-cli.com/) project, run the
-following to install the addon and bower dependencies for bootstrap and
-bootswatch:
+From within your [ember-cli](http://www.ember-cli.com/) project,
+run the following to install this addon:
 
 ```bash
 ember install ember-cli-bootswatch
 ```
-
-Note: This addon _is_ compatible with ember-cli 2.x
 
 
 
@@ -35,20 +44,23 @@ Note: This addon _is_ compatible with ember-cli 2.x
 #### Addon Options
 
 Options for this addon are configured in the projects `ember-cli-build.js` file
-as an 'ember-cli-bootswatch' object property. Available options include:
+as an `'ember-cli-bootswatch'` object property. Available options include:
 
-* `theme` [string]: Name of the Bootswatch theme to be imported, or `'default'` for the standard Bootstrap theme and `'bootstrap'` for the ["visually enhanced"](http://getbootstrap.com/examples/theme/) Bootstrap theme
-* `excludeCSS` [boolean]: By default, the theme's `bootstrap.css` file will be imported
-* `excludeJS` [boolean]: By default, the `bootstrap.js` file will be imported from Bootstrap
-* `excludeFonts` [boolean]: By default, the [font files](https://github.com/thomaspark/bootswatch/tree/gh-pages/fonts) will be imported
+| Option       | Type    | Default     | Description |
+|--------------|---------|-------------|-------------|
+| `theme`      | string  | `'default'` | Name of the Bootswatch theme to be imported, or `'default'` for the standard Bootstap theme |
+| `excludeCSS` | boolean | `false`     | By default, the theme's `bootstrap.css` file will be imported |
+| `excludeJS`  | boolean | `false`     | By default, the `bootstrap.js` file will be imported from Bootstrap |
 
 The only required option is the `theme`. If you do not need to adjust
 any other options, you can just define a string of the theme name
-as the bootswatch options:
+as the ember-cli-bootswatch option:
 
 ```javascript
 // ember-cli-build.js
 /* eslint-env node */
+'use strict';
+
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
@@ -68,13 +80,15 @@ option as an object property:
 ```javascript
 // ember-cli-build.js
 /* eslint-env node */
+'use strict';
+
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'ember-cli-bootswatch': {
-      'theme': 'cerulean',
-      'excludeJS': true
+      theme: 'cerulean',
+      excludeJS: true
     }
   });
 
@@ -82,59 +96,6 @@ module.exports = function(defaults) {
 
   return app.toTree();
 };
-```
-
-
-#### Bootswatch Version
-
-Use [bower to change the version](http://bower.io/docs/api/#install) of
-Bootswatch and Bootstrap that is imported. Be sure to save the new version
-to your `bower.json` file as well. Ex:
-
-```bash
-bower install --save bootswatch#2.3.2 bootstrap#2.3.2
-```
-
-
-#### Usage with ember-cli-less
-
-Bootstrap and Bootswatch both come with [Less](http://lesscss.org/) files
-which you can use with your project instead of the default CSS files. Typically
-you wouldn't do this unless you are already using Less in your project
-elsewhere. You'll need to exclude thedefault CSS files, include the bower
-paths, and finally import the Less files. Ex:
-
-```javascript
-// ember-cli-build.js
-/* eslint-env node */
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-
-module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-    "ember-cli-bootswatch": {
-      theme: "cerulean",
-      excludeCSS: true
-    },
-    lessOptions: {
-      paths: [
-        "bower_components/bootstrap/less",
-        "bower_components/bootswatch"
-      ]
-    }
-  });
-
-  // ... (documentation snipped)
-
-  return app.toTree();
-};
-```
-
-```less
-// app/styles/app.less
-@import "bootstrap";
-@import "themeName/variables";
-@import "themeName/bootswatch";
 ```
 
 
@@ -142,8 +103,34 @@ module.exports = function(defaults) {
 
 ## Usage with other Bootstrap addons
 
-Other Bootstrap addons should be configured NOT to import Bootstrap files
-(styles, themes, fonts, etc.) This way files imported by ember-cli-bootswatch
-do not conflict with other files and versions. But at the same time, if another
-addon requires their own version of a core file (such as JavaScript), then disable
-the import from Bootswatch.
+You can certainly use this addon to just bring in a Bootswatch theme
+but still use components and such from other addons. However, other
+Bootstrap addon's that also import a theme should be configured NOT
+to do so. This way files imported by ember-cli-bootswatch do not
+conflict with other files and versions.
+
+For example, using this addon with `ember-bootstrap`:
+
+```javascript
+// ember-cli-build.js
+/* eslint-env node */
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    'ember-cli-bootswatch': {
+      theme: 'cerulean',
+      excludeJS: true
+    },
+    'ember-bootstrap': {
+      importBootstrapCSS: false
+    }
+  });
+
+  // ... (documentation snipped)
+
+  return app.toTree();
+};
+```
--- index.js
+++ index.js
@@ -3,27 +3,20 @@
 
 
 // module requirements
-var fs    = require('fs');
+var fs   = require('fs');
+var path = require('path');
 
 
 module.exports = {
   name: 'ember-cli-bootswatch',
 
 
-  included: function(app, parentAddon) {
-
-    // Per the ADDON_HOOKS.md document
-    // https://github.com/ember-cli/ember-cli/blob/master/ADDON_HOOKS.md#included
+  included(app) {
     this._super.included.apply(this, arguments);
 
 
-    // Per the ember-cli documentation
-    // http://ember-cli.com/extending/#broccoli-build-options-for-in-repo-addons
-    var target = (parentAddon || app);
-
-
     // Addon options from the apps ember-cli-build.js
-    var options = target.options[this.name] || {};
+    var options = (app.options && app.options[this.name]) || {};
 
 
     // Options can just be a string of the theme,
@@ -37,127 +30,70 @@ module.exports = {
     if (!options.theme) {
       options.theme = 'default';
       console.log(
-        this.name + ': No theme specified, defaulting to the "default" Bootstrap theme. ' +
-        'Define `"theme":"theme-name"` for the "' + this.name + '" options in your ' +
-        'ember-cli-build.js to get rid of this message.'
-      );
-    }
-
-
-    // Other local variables needed
-    var bootstrapPath  = target.bowerDirectory + '/bootstrap/dist';
-    var bootswatchPath = target.bowerDirectory + '/bootswatch';
-    var themePath      = (options.theme === 'default' || options.theme === 'bootstrap' ? bootstrapPath + '/css' : bootswatchPath + '/' + options.theme);
-    var fontsPath      = (options.theme === 'default' || options.theme === 'bootstrap' ? bootstrapPath + '/fonts' : bootswatchPath + '/fonts');
-
-
-    // Make sure bootswatch is available
-    if (!fs.existsSync(bootswatchPath)) {
-      throw new Error(
-        this.name + ': Bootswatch is not available from bower (' + bootswatchPath + '), ' +
-        'install into your project by running `bower install bootswatch --save`'
-      );
-    }
-
-
-    // Make sure bootstrap is available
-    if (!options.excludeJS && !fs.existsSync(bootstrapPath)) {
-      throw new Error(
-        this.name + ': Bootstrap is not available from bower (' + bootstrapPath + '), ' +
-        'install into your project by running `bower install bootstrap --save`'
-      );
-    }
-
-
-    // Fail if theme does not exist
-    if (!fs.existsSync(themePath)) {
-      throw new Error(
-        this.name + ': Theme (' + options.theme + ') is not available, directory not found [' + themePath + ']'
+        `${this.name}: No theme specified, defaulting to the standard Bootstrap theme. ` +
+        `Define a "${this.name}":"theme-name" in your 'ember-cli-build.js' to get rid of this message.`
       );
     }
 
 
     // Friendly message if the addon will not do anything
-    if (options.excludeFonts && options.excludeJS && options.excludeCSS) {
+    if (options.excludeJS && options.excludeCSS) {
       console.error(
-        this.name + ': All exclude options are enabled (excludeCSS, excludeJS, excludeFonts). ' +
-        'This addon will not import anything into your build tree, which may be intended if ' +
-        'you plan on only using Sass or Less files.'
+        `${this.name}: All exclude options are enabled (excludeCSS, excludeJS). ` +
+        'This addon will not import anything into your build tree, which may be intended ' +
+        'if you plan on only using Sass files.'
       );
     }
 
 
-    // Include bootstrap fonts by default, opt-out option
-    if (!options.excludeFonts) {
-
-
-      // Get all of the font files
-      var fontsToImport = fs.readdirSync(fontsPath);
-      var filesInFonts  = []; // Bucket for filenames already in the fonts folder
-      var fontsSkipped  = []; // Bucket for fonts not imported because they already have been
-
-
-      // Find files already imported into the fonts folder
-      target.otherAssetPaths.forEach(function(asset){
-        if (asset.dest == '/fonts') {
-          filesInFonts.push(asset.file);
-        }
-      });
-
-
-      // Attempt to import each font, if not already imported
-      fontsToImport.forEach(function(fontFilename){
-        if (filesInFonts.indexOf(fontFilename) > -1) {
-          fontsSkipped.push(fontFilename);
-        } else {
-          target.import(fontsPath + '/' + fontFilename, {destDir:'/fonts'});
-        }
-      });
-
-
-      // Fonts that had already been imported, so bootswatch skipped..
-      // But do not error if bootstrap option incorrect, already logged similar error
-      if (fontsSkipped.length) {
-        console.error(
-          this.name + ': Fonts already imported [' + fontsSkipped.join(', ') +
-          '] by another addon or in your ember-cli-build.js, disable the import ' +
-          'from other locations or disable the bootswatch import by setting ' +
-          '`"excludeFonts":true` for the "' + this.name + '" options in your ember-cli-build.js'
-        );
-      }
-
-
-    } // if (!options.excludeFonts)
-
-
-    // Include bootstrap js by default, opt-out option
+    // Include Bootstrap JavaScript by default, opt-out as an option
     if (!options.excludeJS) {
-      target.import({
-        development: bootstrapPath + '/js/bootstrap.js',
-        production:  bootstrapPath + '/js/bootstrap.min.js'
-      });
+      this.import(
+        path.join('node_modules', 'bootstrap', 'dist', 'js', 'bootstrap.js')
+      );
     }
 
 
-    // Include bootswatch css by default, opt-out option
+    // Include Bootswatch CSS by default, opt-out as an option
     if (!options.excludeCSS) {
-      target.import({
-        development: themePath + '/bootstrap.css',
-        production:  themePath + '/bootstrap.min.css'
-      });
 
-      // The 'bootstrap' theme also needs another file
-      if (options.theme === 'bootstrap') {
-        target.import({
-          development: themePath + '/bootstrap-theme.css',
-          production:  themePath + '/bootstrap-theme.min.css'
-        });
-      }
+      // If not the "default" bootstrap theme,
+      // ensure the bootswatch theme exists
+      if (options.theme !== 'default') {
+
+        var nodePath = path.dirname(
+          require.resolve('bootswatch/package.json')
+        );
+
+        var availableThemes = fs.readdirSync(
+          path.join( nodePath, 'dist' )
+        );
+
+        // Fail if theme does not exist
+        if (!availableThemes.includes(options.theme)) {
+          throw new Error(
+            `${this.name}: Theme (${options.theme}) is not available, ` +
+            ` not listed as an option from bootswatch; default, ${availableThemes.join(', ')}.`
+          );
+        }
+
+      } // if (options.theme !== 'default')
+
+      // Determine the theme CSS path
+      var themePath = (
+        options.theme === 'default' ?
+        path.join('node_modules', 'bootstrap', 'dist', 'css') :
+        path.join('node_modules', 'bootswatch', 'dist', options.theme)
+      );
+
+      this.import(
+        path.join(themePath, 'bootstrap.css')
+      );
 
     } // if (!options.excludeCSS)
 
 
-  } // :included
+  } // included()
 
 
 }; // module.exports
